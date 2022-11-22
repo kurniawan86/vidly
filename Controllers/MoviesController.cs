67620af5439ce85bbafb6600af88e6678c15d5ca
kurniawan86 <90589478+kurniawan86@users.noreply.github.com>
2022-11-23 03:50:26 +0700

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Sherk!" };
+
+            //return view(movie);
+            //return Content("hello world!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction(
+                "index", "home", new
+                {
+                    page = 1,
+                    sortby = "name"
+                });
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(
+                String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+    }
+}
